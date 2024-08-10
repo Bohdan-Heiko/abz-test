25c2d6b614e458979951cefce669b87fe3b0b3b8
gektor <bheiko@salerow.software>
2024-08-10 14:13:38 +0300

    abz-users_page: refactoring users page

---
+++ src/screens/users/_hooks/useUserData.ts
@@ -0,0 +1,34 @@
+import { useLazyGetAllUsersQuery } from "@/store/services/users-api";
+import { UsersResponse } from "@/types/users";
+import { useRef, useState } from "react";
+import { Alert } from "react-native";
+
+interface ReturnData {
+  users: UsersResponse["users"];
+  pageRef: React.MutableRefObject<number>;
+  loadMoreData: (page: number) => Promise<void>;
+  isUsersLoading: boolean;
+}
+export const useUserData = (): ReturnData => {
+  const pageRef = useRef<number>(1);
+  const [users, setUsers] = useState<UsersResponse["users"]>([]);
+  const [getAllUsers, { isFetching: isUsersLoading }] = useLazyGetAllUsersQuery();
+
+  const loadMoreData = async (page: number) => {
+    if (isUsersLoading) return;
+
+    await getAllUsers({ count: 3, page })
+      .unwrap()
+      .then((res) => {
+        setUsers((prev) => [...prev, ...res.users]);
+      })
+      .catch(() => Alert.alert("Something went wrong"));
+  };
+
+  return {
+    users,
+    pageRef,
+    loadMoreData,
+    isUsersLoading,
+  };
+};
--- src/screens/users/index.tsx
+++ src/screens/users/index.tsx
@@ -1,42 +1,17 @@
 import { WorkingRequest } from "@/shared/components/work-request";
 import { ScreenContainer } from "@/shared/ui-kit/screen-container";
-import { useLazyGetAllUsersQuery } from "@/store/services/users-api";
 import { UsersResponse } from "@/types/users";
 import { FlashList } from "@shopify/flash-list";
-import { useCallback, useRef, useState } from "react";
-import { ActivityIndicator, Alert, Dimensions, StyleSheet, View } from "react-native";
+import { useCallback } from "react";
+import { ActivityIndicator, Dimensions, StyleSheet, View } from "react-native";
 import { NoUsers } from "./_components/no-users";
 import { User } from "./_components/user";
+import { useUserData } from "./_hooks/useUserData";
 
 export const Users = () => {
-  const pageRef = useRef<number>(1);
-  const [users, setUsers] = useState<UsersResponse["users"]>([]);
-  const [getAllUsers, { data: usersData, isFetching: isUsersLoading }] = useLazyGetAllUsersQuery();
+  const { isUsersLoading, loadMoreData, users, pageRef } = useUserData();
   const KEY_EXTRACTOR = useCallback((item: UsersResponse["users"][0]) => item.id.toString(), []);
 
-  const loadMoreData = async (page: number) => {
-    if (isUsersLoading) return;
-
-    await getAllUsers({ count: 3, page })
-      .unwrap()
-      .then((res) => {
-        setUsers((prev) => [...prev, ...res.users]);
-      })
-      .catch((e) => {
-        console.log(e);
-
-        Alert.alert("Something went wrong");
-      });
-  };
-
-  const renderFooter = () => {
-    if (!isUsersLoading) return null;
-    return (
-      <View style={{ paddingVertical: 20 }}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
-  };
   return (
     <ScreenContainer>
       <WorkingRequest requestType="GET" />
@@ -57,7 +32,7 @@ export const Users = () => {
           onLoad={() => loadMoreData(pageRef.current)} // STARTED IN FIRST LOADING
           onEndReached={() => loadMoreData((pageRef.current += 1))} // STARTED IN WHEN WE SCROLLING TO CENTER OF COMPONENT
           renderItem={({ item }) => <User user={item} />}
-          ListFooterComponent={renderFooter}
+          ListFooterComponent={() => <RenderFooter isUsersLoading={isUsersLoading} />}
           ListEmptyComponent={() => (
             <View style={styles.emptyComponentContainer}>
               <NoUsers />
@@ -66,6 +41,15 @@ export const Users = () => {
         />
       </View>
     </ScreenContainer>
+  );
+};
+
+const RenderFooter = ({ isUsersLoading }: { isUsersLoading: boolean }) => {
+  if (!isUsersLoading) return null;
+  return (
+    <View style={{ paddingVertical: 20 }}>
+      <ActivityIndicator size="large" />
+    </View>
   );
 };
 
